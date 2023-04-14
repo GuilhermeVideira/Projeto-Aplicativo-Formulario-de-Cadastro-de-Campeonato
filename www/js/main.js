415ce789b9d10cff953d8cfdcd397c12f4e3efd4
GuilhermeVideira <guifvideira@gmail.com>
2023-04-15 00:12:25 +0900

    Project's Initial Commit

---
+++ www/js/main.js
@@ -0,0 +1,65 @@
+var historico = "";
+
+function exibir(){
+
+    var nome, email, jogo, jogonome
+
+    nome = document.getElementById('insertnome').value
+    email = document.getElementById('email').value
+    jogo = document.getElementById('jogo').value
+    jogonome = document.getElementById('nomejogo').value
+
+    historico = "Nome: " + nome, + "Email: " + email, "Tipo de jogo: " + jogo , "Nome do jogo: " + jogonome; 
+
+    document.getElementById('exibirCadastro').value = historico;
+
+}
+
+var cadastros
+var nomesvetor = [], emailsvetor = [], jogosvetor  = [], jogosnomevetor = []; 
+
+function exibirtodos(){
+
+    var nomeDigitado, emailDigitado, jogoDigitado, jogonomeDigitado
+
+    nomeDigitado = document.getElementById('insertnome').value
+    emailDigitado = document.getElementById('email').value
+    jogoDigitado = document.getElementById('jogo').value
+    jogonomeDigitado = document.getElementById('nomejogo').value
+
+    nomesvetor.push(nomeDigitado)
+    emailsvetor.push(emailDigitado)
+    jogosvetor.push(jogoDigitado)
+    jogosnomevetor.push(jogonomeDigitado)
+    
+  cadastros = (nomesvetor[qt]), (emailsvetor[qt]), (jogosvetor[qt]), (jogosnomevetor[qt])
+
+    document.getElementById('exibirTodosCadastro').value = cadastros
+
+}
+
+function cadastrar(){
+
+    var nomeInserido, qt;
+
+    nomeInserido = document.getElementById('insertnome').value
+
+    nomesvetor.push(nomeInserido)
+
+    for (qt = 0; qt < nomesvetor.length; qt++)
+    {
+      alert(nomesvetor[qt])
+    }
+        
+}
+
+function limpar() {
+var ctx = quadro.getContext("exibirTodosCadastro")
+ctx.clearRect();
+}
+
+function limpar() {
+var ctx = quadro.getContext("exibirCadastro")
+ctx.clearRect();
+}
+
